f000d121c0c9fcd43b86ad332b897e916332a1a2
Luan Dutra Carneiro <luan.carneiro2@aluno.unip.br>
2024-09-13 18:58:25 +0000

    t

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -10,6 +10,9 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<PizzaContext>(options =>
     options.UseSqlite("Data Source=ContosoPizza.db"));
 
+
+builder.Services.AddScoped<PizzaService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
